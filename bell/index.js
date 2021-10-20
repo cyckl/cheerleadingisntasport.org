c4452dfd8e09335020f8b2ce42e8f343deb552af
cyckl <git@cyckl.net>
2021-10-20 10:55:32 -0700

    add toggle functionality to show button

--- bell/index.js
+++ bell/index.js
@@ -1,8 +1,3 @@
-// Hide all elements on JS load instead of in HTML to add support for non-javascript environments
-document.getElementById("std").style.display="none";
-document.getElementById("long").style.display="none";
-document.getElementById("min").style.display="none";
-
 var d = new Date();
 
 const weekday = new Array(7);
@@ -19,10 +14,25 @@ weekday[6] = "Saturday";
 
 const minDays = ["09-17", "10-18", "11-15", "02-28", "03-21", "04-18"];
 
-// Check if minimum day, if not, proceed normal
-if (minDays.includes((d.getMonth() + 1) + "-" + d.getDate())) {
-	document.getElementById("min").style.display="block";
-} else {
+// Default "show" state
+var state = false;
+
+// Hide all elements on JS load instead of in HTML to add support for non-javascript environments
+function load() {
+	// TODO: Combine this into one selector
+	document.getElementById("std").style.display="none";
+	document.getElementById("long").style.display="none";
+	document.getElementById("min").style.display="none";
+}
+
+// Hide everything then show relevant elements
+function hide() {
+	// Hide all
+	load();
+	// Check if minimum day, if not, proceed normal
+	if (minDays.includes((d.getMonth() + 1) + "-" + d.getDate())) {
+		document.getElementById("min").style.display="block";
+	} else {
 	// Check day of the week and show relevant schedule
 	switch(d.getDay()) {
 		case 1:
@@ -36,15 +46,37 @@ if (minDays.includes((d.getMonth() + 1) + "-" + d.getDate())) {
 			break;
 		case 6:
 		case 0:
+			// TODO: Combine this into one selector
 			document.getElementById("std").style.display="block";
 			document.getElementById("long").style.display="block";
 			document.getElementById("min").style.display="block";
 			break;
+		}
+	}
+	// Flip state bit to be false
+	state = false;
+}
+
+// Show all elements
+function show() {
+	// TODO: Combine this into one selector
+	document.getElementById("std").style.display = "block";
+	document.getElementById("long").style.display = "block";
+	document.getElementById("min").style.display = "block";
+	// Flip state bit to be true
+	state = true;
+}
+
+// Run on button press and run relevant function
+function button() {
+	if (!state) {
+		show();
+		document.getElementById("view").innerHTML = "Hide all";
+	} else {
+		hide();
+		document.getElementById("view").innerHTML = "Show all";
 	}
 }
 
-function show() {
-	document.getElementById("std").style.display="block";
-	document.getElementById("long").style.display="block";
-	document.getElementById("min").style.display="block";
-}
+// Set up page
+hide();
