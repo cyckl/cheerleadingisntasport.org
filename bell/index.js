ab24cf410daba14491d6b8a5b65aa3d375269553
cyckl <cyckl@pm.me>
2022-01-14 10:32:26 -0800

    add final days support

--- bell/index.js
+++ bell/index.js
@@ -1,6 +1,8 @@
-var d = new Date();
+var d = new Date(2022, 0, 20);
 
-const minDays = ["09-17", "10-18", "11-15", "02-28", "03-21", "04-18"];
+const minDays = ["9-17", "10-18", "11-15", "2-28", "3-21", "4-18"];
+const finalDays = ["1-19", "1-20", "1-21", "6-10", "6-13", "6-14"];
+
 
 // Default "show" state
 var state = false;
@@ -11,6 +13,7 @@ function load() {
 	document.getElementById("std").style.display="none";
 	document.getElementById("long").style.display="none";
 	document.getElementById("min").style.display="none";
+	document.getElementById("final").style.display="none";
 }
 
 // Hide everything then show relevant elements
@@ -20,6 +23,8 @@ function hide() {
 	// Check if minimum day, if not, proceed normal
 	if (minDays.includes((d.getMonth() + 1) + "-" + d.getDate())) {
 		document.getElementById("min").style.display="block";
+	} else if (finalDays.includes((d.getMonth() + 1) + "-" + d.getDate())) {
+		document.getElementById("final").style.display="block";
 	} else {
 	// Check day of the week and show relevant schedule
 	switch(d.getDay()) {
@@ -38,6 +43,7 @@ function hide() {
 			document.getElementById("std").style.display="block";
 			document.getElementById("long").style.display="block";
 			document.getElementById("min").style.display="block";
+			document.getElementById("final").style.display="block";
 			break;
 		}
 	}
@@ -53,6 +59,7 @@ function show() {
 	document.getElementById("std").style.display = "block";
 	document.getElementById("long").style.display = "block";
 	document.getElementById("min").style.display = "block";
+	document.getElementById("final").style.display="block";
 	// Flip state bit to be true
 	state = true;
 }
